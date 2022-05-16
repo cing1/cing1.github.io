ccb32573c7cbbcecdd6d1bdd49a86f207b983196
Mario <mariocs855@gmail.com>
2022-05-16 03:52:35 -0400

    responsive work

--- portfolio/assets/js/main.js
+++ portfolio/assets/js/main.js
@@ -14,7 +14,7 @@
         });
     }
 
-    // Smooth scroll for the navigation menu and links with .scrollto classes
+    // Smooth scroll for the navigation menu and links with .scroll to classes
     $(document).on('click', '.nav-menu a, .scrollto', function(e) {
         if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
             e.preventDefault();
