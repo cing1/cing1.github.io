da0b206b16019162da310db4b35c9b3c0e7d2bdd
cing1 <mariocs855@gmail.com>
2025-03-21 08:44:31 -0400

    fixed errors

--- src/navbar.js
+++ src/navbar.js
@@ -16,7 +16,7 @@ class menubar extends HTMLElement {
           <li><a class="menu" href="https://cing1.github.io/#services">Servicios</a></li>
           <li><a class="menu" href="https://cing1.github.io/#projects">Proyectos</a></li>
           <li><a class="menu" href="https://cing1.github.io/#contact">Contacto</a></li>
-          <li><a class="menu" id="mi-blog" href="https://cing1.github.io/blog">Blog</a></li>
+          <li><a class="menu" id="mi-blog" href="https://cing1.github.io/pages/blog.html">Blog</a></li>
           <li><a class="menu" href="https://cing1.github.io/pages/portfolio/mario_cesgo_soliz.html" target="_blank">Portfolio</a></li>
           <li>
             <a class='menu' id='recursos' href="#">Recursos</a>
