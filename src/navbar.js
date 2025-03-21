9d4f4922bd943144ecad85162387eb40bad746f1
cing1 <mariocs855@gmail.com>
2025-03-21 08:34:41 -0400

    update 02

--- src/navbar.js
+++ src/navbar.js
@@ -17,7 +17,7 @@ class menubar extends HTMLElement {
           <li><a class="menu" href="https://cing1.github.io/#projects">Proyectos</a></li>
           <li><a class="menu" href="https://cing1.github.io/#contact">Contacto</a></li>
           <li><a class="menu" id="mi-blog" href="https://cing1.github.io/blog">Blog</a></li>
-          <li><a class="menu" href="https://cing1.github.io/portfolio/mario_cesgo_soliz" target="_blank">Portfolio</a></li>
+          <li><a class="menu" href="https://cing1.github.io/pages/portfolio/mario_cesgo_soliz" target="_blank">Portfolio</a></li>
           <li>
             <a class='menu' id='recursos' href="#">Recursos</a>
             <ul>
