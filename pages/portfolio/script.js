cd2efc9493137b7e04269fcd8fb80063ca0151af
cing1 <mariocs855@gmail.com>
2025-12-10 13:33:21 -0400

    actualizacion de mi portafolio

---
+++ pages/portfolio/script.js
@@ -0,0 +1,321 @@
+
+const proyectos = [
+    {
+        id: 1, 
+        titulo: "Diseño geométrico de carretera, tramo doble vía Sucre-Yamparáez",
+        descripcion: "Modelado topográfico, alineamientos, perfiles y diseño geométrico completo.",
+        imagen: "./img/proyectos/1-tramo_doble_via_sucre-yamparaez/portada.png",
+        enlace: "#",
+        etiquetas: ["Civil 3D", "Carreteras", "BIM", "Topografía", "Diseño Vial"],
+        mencion: "carreteras",
+        fecha: "2025-07-10",
+        galeria: [
+            "./img/proyectos/1-tramo_doble_via_sucre-yamparaez/imagen_1.png",
+            "./img/proyectos/1-tramo_doble_via_sucre-yamparaez/imagen_2.png",
+            "./img/proyectos/1-tramo_doble_via_sucre-yamparaez/imagen_3.png",
+            "./img/proyectos/1-tramo_doble_via_sucre-yamparaez/imagen_4.png",
+            "./img/proyectos/1-tramo_doble_via_sucre-yamparaez/imagen_5.png",
+            "./img/proyectos/1-tramo_doble_via_sucre-yamparaez/imagen_6.png",
+            "./img/proyectos/1-tramo_doble_via_sucre-yamparaez/imagen_7.png",
+            "./img/proyectos/1-tramo_doble_via_sucre-yamparaez/imagen_8.png",
+            "./img/proyectos/1-tramo_doble_via_sucre-yamparaez/imagen_9.png",
+            "./img/proyectos/1-tramo_doble_via_sucre-yamparaez/imagen_10.png",
+            "./img/proyectos/1-tramo_doble_via_sucre-yamparaez/imagen_11.png"
+        ]
+    },
+    {
+        id: 2, 
+        titulo: "Análisis, Diseño y Modelado BIM de Puente Postensado",
+        descripcion: "Cálculo estructural según AASHTO, diseño de postensado y modelado BIM completo de puente.",
+        imagen: "./img/proyectos/2-analisis_diseño_puente_postensado/portada.jpg",
+        enlace: "#",
+        etiquetas: ["Puentes", "Postensado", "AASHTO", "SAP2000", "BIM", "Infraestructura", "CSI Bridge"],
+        mencion: "estructural",
+        fecha: "2025-08-01",
+        galeria: [
+            "https://picsum.photos/1000/600?project=4-img1",
+            "https://picsum.photos/1000/600?project=4-img2",
+            "https://picsum.photos/1000/600?project=4-img3"
+        ]
+    },
+    {
+        id: 3, 
+        titulo: "Simulación 4D de un edificio con Navisworks",
+        descripcion: "Integración BIM + cronograma para visualizar fases constructivas y detectar interferencias.",
+        imagen: "./img/proyectos/3-simulacion_4d_de_edificio_con_navisworks/portada.jpg",
+        enlace: "#",
+        etiquetas: ["Navisworks", "BIM", "4D", "Construcción", "Cronograma", "Coordinación"],
+        mencion: "estructural",
+        fecha: "2025-09-01",
+        galeria: [
+            "./img/proyectos/3-simulacion_4d_de_edificio_con_navisworks/imagen_1.png",
+            "./img/proyectos/3-simulacion_4d_de_edificio_con_navisworks/imagen_2.png"
+        ]
+    },
+    {
+        id: 4, 
+        titulo: "Diseño Geométrico de Carreteras implementando metodología BIM",
+        descripcion: "Alineamiento horizontal y vertical, peraltes, secciones transversales y movimiento de tierras.",
+        imagen: "./img/proyectos/4-diseno_geometrico_de_carreteras_implementando_metodologia_bim/portada.jpg",
+        enlace: "#",
+        etiquetas: ["BIM","Civil 3D", "Carreteras","Infraworks","Navisworks", "Diseño Vial", "Fotogrametría", "Cronograma", "Infraestructura"],
+        mencion: "carreteras",
+        fecha: "2025-12-03",
+        galeria: [
+            "./img/proyectos/4-diseno_geometrico_de_carreteras_implementando_metodologia_bim/imagen_1.jpg",
+            "./img/proyectos/4-diseno_geometrico_de_carreteras_implementando_metodologia_bim/imagen_2.jpg",
+            "./img/proyectos/4-diseno_geometrico_de_carreteras_implementando_metodologia_bim/imagen_3.jpg",
+            "./img/proyectos/4-diseno_geometrico_de_carreteras_implementando_metodologia_bim/imagen_4.jpg",
+            "./img/proyectos/4-diseno_geometrico_de_carreteras_implementando_metodologia_bim/imagen_5.jpg",
+            "./img/proyectos/4-diseno_geometrico_de_carreteras_implementando_metodologia_bim/imagen_6.jpg",
+            "./img/proyectos/4-diseno_geometrico_de_carreteras_implementando_metodologia_bim/imagen_7.jpg",
+            "./img/proyectos/4-diseno_geometrico_de_carreteras_implementando_metodologia_bim/imagen_8.jpg",
+            "./img/proyectos/4-diseno_geometrico_de_carreteras_implementando_metodologia_bim/imagen_9.jpg",
+            "./img/proyectos/4-diseno_geometrico_de_carreteras_implementando_metodologia_bim/imagen_10.jpg"
+        ]
+    },
+    {
+        id: 5, 
+        titulo: "Diseño y Modelado de Estructura Metálica",
+        descripcion: "Diseño de conexiones, análisis de cargas y modelado 3D de estructura metálica tipo tinglado.",
+        imagen: "./img/proyectos/5-diseno_y_modelado_de_estructura_metalica/portada.jpg",
+        enlace: "#",
+        etiquetas: ["Acero", "SAP2000", "Estructuras Metálicas", "AISC","Revit","Robot Structural Analysis","Viento", "Conexiones"],
+        mencion: "estructural",
+        fecha: "2023-10-15",
+        galeria: [
+            "./img/proyectos/5-diseno_y_modelado_de_estructura_metalica/imagen_1.jpg",
+            "./img/proyectos/5-diseno_y_modelado_de_estructura_metalica/imagen_2.jpg",
+            "./img/proyectos/5-diseno_y_modelado_de_estructura_metalica/imagen_3.jpg",
+            "./img/proyectos/5-diseno_y_modelado_de_estructura_metalica/imagen_4.jpg"
+        ]
+    },
+    {
+        id: 6, 
+        titulo: "Diseño de red de alcantarillado sanitario urbano",
+        descripcion: "Proyecto integral que incluye el análisis de caudales, modelación hidráulica, trazo de colectores y elaboración de planos técnicos para la red de alcantarillado sanitario urbano.",
+        imagen: "./img/proyectos/6-sistema_de_drenaje_sanitario/portada.jpg",
+        enlace: "#",
+        etiquetas: ["Alcantarillado", "Drenaje Sanitario", "Hidráulica", "Infraestructura Urbana", "Civil 3D", "Saneamiento","Excel"],
+        mencion: "sanitaria",
+        fecha: "2024-06-17",
+        galeria: [
+            "./img/proyectos/6-sistema_de_drenaje_sanitario/imagen_1.jpg",
+            "./img/proyectos/6-sistema_de_drenaje_sanitario/imagen_2.jpg",
+            "./img/proyectos/6-sistema_de_drenaje_sanitario/imagen_3.jpg",
+            "./img/proyectos/6-sistema_de_drenaje_sanitario/imagen_4.jpg",
+            "./img/proyectos/6-sistema_de_drenaje_sanitario/imagen_5.jpg",
+            "./img/proyectos/6-sistema_de_drenaje_sanitario/imagen_6.jpg",
+            "./img/proyectos/6-sistema_de_drenaje_sanitario/imagen_7.jpg",
+            "./img/proyectos/6-sistema_de_drenaje_sanitario/imagen_8.jpg",
+            "./img/proyectos/6-sistema_de_drenaje_sanitario/imagen_9.jpg",
+            "./img/proyectos/6-sistema_de_drenaje_sanitario/imagen_10.jpg",
+            "./img/proyectos/6-sistema_de_drenaje_sanitario/imagen_11.jpg",
+            "./img/proyectos/6-sistema_de_drenaje_sanitario/imagen_12.jpg"
+            
+        ]
+    },
+    {
+        id: 7, 
+        titulo: "Modelado Estructural Bahira Gran Hotel",
+        descripcion: "Modelado estructura de edificio de concreto armado.",
+        imagen: "./img/proyectos/7-modelado_est_bahria_grand_hotel/portada.jpg",
+        enlace: "#",
+        etiquetas: ["Revit", "Concreto Armado", "Diseño Estructural", "Edificios", "Modelado 3D", "BIM"],
+        mencion: "estructural",
+        fecha: "2022-11-20",
+        galeria: [
+            "./img/proyectos/7-modelado_est_bahria_grand_hotel/imagen_1.jpg",
+            "./img/proyectos/7-modelado_est_bahria_grand_hotel/imagen_2.jpg",
+            "./img/proyectos/7-modelado_est_bahria_grand_hotel/imagen_3.jpg"
+        ]
+    },
+    {
+        id: 8, 
+        titulo: "Modelado estructura Stanley Condominium",
+        descripcion: "Modelado estructura de edificio de concreto armado",
+        imagen: "./img/proyectos/8-modelado_est_stanley_condominium/portada.jpg",
+        enlace: "#",
+        etiquetas: ["Revit", "BIM", "Edificio", "Concreto Armado", "Diseño Estructural", "Modelado 3D",],
+        mencion: "estructural",
+        fecha: "2021-12-05",
+        galeria: [
+            "./img/proyectos/8-modelado_est_stanley_condominium/imagen_1.jpg",
+            "./img/proyectos/8-modelado_est_stanley_condominium/imagen_2.jpg",
+            "./img/proyectos/8-modelado_est_stanley_condominium/imagen_3.jpg",
+            "./img/proyectos/8-modelado_est_stanley_condominium/imagen_4.jpg"
+        ]
+    },
+    {
+        id: 9, 
+        titulo: "Modelado BIM de Edificio Multifamiliar",
+        descripcion: "Modelado BIM integral de un edificio multifamiliar: creación del modelo arquitectónico, estructural e instalaciones hidrosanitarias; coordinación interdisciplinaria (arquitectura, estructura e instalaciones), generación de planos constructivos y tablas de cantidades, y preparación de entregables BIM para obra.",
+        imagen: "./img/proyectos/9-modelado_bim_edificio_multifamiliar/portada.jpg",
+        enlace: "#",
+        etiquetas: ["Revit", "BIM", "Edificio", "Concreto Armado", "Diseño Estructural", "Modelado 3D",],
+        mencion: "estructural",
+        fecha: "2025-12-10",
+        galeria: [
+            "./img/proyectos/9-modelado_bim_edificio_multifamiliar/imagen_1.jpg",
+            "./img/proyectos/9-modelado_bim_edificio_multifamiliar/imagen_2.jpg",
+            "./img/proyectos/9-modelado_bim_edificio_multifamiliar/imagen_3.jpg",
+            "./img/proyectos/9-modelado_bim_edificio_multifamiliar/imagen_4.jpg",
+            "./img/proyectos/9-modelado_bim_edificio_multifamiliar/imagen_5.jpg",
+            "./img/proyectos/9-modelado_bim_edificio_multifamiliar/imagen_6.jpg",
+            "./img/proyectos/9-modelado_bim_edificio_multifamiliar/imagen_7.jpg"
+        
+        ]
+    },
+];
+// Función para manejar el clic en "Ver más"
+function verProyecto(proyectoId) {
+    const proyecto = proyectos.find(p => p.id === proyectoId);
+    
+    if (proyecto) {
+        // Guarda los datos del proyecto en localStorage
+        localStorage.setItem('proyectoSeleccionado', JSON.stringify(proyecto));
+        
+        // Redirige a la página de detalle
+        window.location.href = 'proyecto.html'; 
+    } else {
+        alert('Proyecto no encontrado.');
+    }
+}
+
+const contenedor = document.getElementById("lista-proyectos");
+const totalProyectosEl = document.getElementById("total-proyectos");
+const selectOrdenar = document.getElementById("ordenar");
+
+let mencionActiva = "todos";
+let terminoBusqueda = "";
+let ordenActivo = "reciente";
+
+function formatoFecha(fechaString) {
+    const [year, month, day] = fechaString.split('-');
+    return `${day}/${month}/${year}`;
+}
+
+function actualizarContador(cantidad) {
+    totalProyectosEl.textContent = cantidad;
+}
+
+function filtrarYOrdenarProyectos() {
+    let proyectosFiltrados = proyectos;
+
+    // 1. Filtrar por mención
+    if (mencionActiva !== "todos") {
+        proyectosFiltrados = proyectosFiltrados.filter(p => p.mencion === mencionActiva);
+    }
+
+    // 2. Filtrar por búsqueda
+    if (terminoBusqueda) {
+        proyectosFiltrados = proyectosFiltrados.filter(p => {
+            const busqueda = terminoBusqueda.toLowerCase();
+            const tituloCoincide = p.titulo.toLowerCase().includes(busqueda);
+            const descripcionCoincide = p.descripcion.toLowerCase().includes(busqueda);
+            const etiquetasCoinciden = p.etiquetas.some(tag => 
+                tag.toLowerCase().includes(busqueda)
+            );
+            
+            return tituloCoincide || descripcionCoincide || etiquetasCoinciden;
+        });
+    }
+
+    // 3. Ordenar por fecha
+    proyectosFiltrados.sort((a, b) => {
+        const fechaA = new Date(a.fecha);
+        const fechaB = new Date(b.fecha);
+
+        if (ordenActivo === "reciente") {
+            return fechaB - fechaA;
+        } else {
+            return fechaA - fechaB;
+        }
+    });
+
+    renderizarProyectos(proyectosFiltrados);
+}
+
+function renderizarProyectos(proyectosFiltrados) {
+    contenedor.innerHTML = "";
+    actualizarContador(proyectosFiltrados.length);
+    
+    if (proyectosFiltrados.length === 0) {
+        contenedor.innerHTML = '<p style="text-align:center; color: var(--text); padding: 2rem;">No se encontraron proyectos con los filtros o la búsqueda seleccionada</p>';
+        return;
+    }
+
+    proyectosFiltrados.forEach(p => {
+        const div = document.createElement("div");
+        div.className = "card proyecto";
+        
+        const etiquetasHTML = p.etiquetas.map(tag => `<span class="tag">${tag}</span>`).join('');
+        const fechaLegible = formatoFecha(p.fecha);
+        
+        // El botón "Ver más" llama a la función JS
+        div.innerHTML = `
+            <div class="card_container">
+                <h3>${p.titulo}</h3>
+                <p>${p.descripcion}</p>
+                <p class="fecha-publicacion">
+                    <span style="font-size: 1.2em; line-height: 1;">🗓️</span> 
+                    Publicado el: <strong>${fechaLegible}</strong>
+                </p>
+                <div class="tags">${etiquetasHTML}</div>
+                <a class="btn" onclick="verProyecto(${p.id})">Ver más</a> 
+            </div>
+            <img src="${p.imagen}" alt="Proyecto de ingeniería" />
+        `;
+        contenedor.appendChild(div);
+    });
+}
+
+// --- Event Listeners ---
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Inicializar filtros y proyectos
+    filtrarYOrdenarProyectos();
+
+    // MODO CLARO/OSCURO (Lógica de Toggle - Oscuro es el principal)
+    const btnModoOscuro = document.getElementById("modoOscuroBtn");
+    
+    // Función de inicialización
+    const inicializarBoton = () => {
+        // Inicializa el texto del botón basándose en la clase 'light' que puede estar en el body.
+        btnModoOscuro.innerText = document.body.classList.contains("light") ? "🌒" : "🔆";
+    };
+
+    // Aplicar inicialización del texto del botón
+    inicializarBoton();
+
+    // Listener para el botón: Ahora solo hace el toggle
+    btnModoOscuro.addEventListener("click", () => {
+        const isLight = document.body.classList.toggle("light");
+        btnModoOscuro.innerText = isLight ? "🌒" : "🔆";
+    });
+
+    // FILTROS POR MENCIÓN
+    const filtrosBtns = document.querySelectorAll(".filtro-btn");
+    filtrosBtns.forEach(btn => {
+        btn.addEventListener("click", () => {
+            filtrosBtns.forEach(b => b.classList.remove("active"));
+            btn.classList.add("active");
+            
+            mencionActiva = btn.dataset.mencion;
+            filtrarYOrdenarProyectos();
+        });
+    });
+
+    // BUSCADOR
+    const buscador = document.getElementById("buscador");
+    buscador.addEventListener("input", (e) => {
+        terminoBusqueda = e.target.value.trim().toLowerCase();
+        filtrarYOrdenarProyectos();
+    });
+
+    // ORDENACIÓN POR FECHA
+    selectOrdenar.addEventListener("change", (e) => {
+        ordenActivo = e.target.value;
+        filtrarYOrdenarProyectos();
+    });
+});
